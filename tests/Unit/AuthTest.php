065ff0d8c7fc4cac4632f3d0586646a1572e3061
iLyas Farawe <faraweilyas@gmail.com>
2020-05-20 22:25:37 +0100

    Clean up test cases

--- tests/Unit/AuthTest.php
+++ tests/Unit/AuthTest.php
@@ -8,13 +8,17 @@ use PHPUnit\Framework\TestCase;
 
 class AuthTest extends TestCase
 {
-	public function test_can_create_new_auth_object_and_test_two_equal_strings ()
+	public function test_can_create_new_auth_object()
 	{
 		$this->assertInstanceOf(Auth::class, new Auth());
+	}
+
+	public function test_can_test_two_equal_strings()
+	{
 		$this->assertTrue(Auth::strictCheck("first", "first"));
 	}
 
-	public function test_can_test_two_strings_that_are_not_equal ()
+	public function test_can_test_two_strings_that_are_not_equal()
 	{
 		$this->assertFalse(Auth::strictCheck("first", "second"));
 	}

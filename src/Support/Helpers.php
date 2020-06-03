818d69876067e0fe62720194c442c8681e3a0c4a
iLyas Farawe <faraweilyas@gmail.com>
2020-06-03 02:13:00 +0100

    Code clean up

--- src/Support/Helpers.php
+++ src/Support/Helpers.php
@@ -1091,7 +1091,7 @@ if (!function_exists('generateRandomToken')):
 	{
 		if ($length <= 0) return "";
 		if (version_compare(PHP_VERSION, "7.0.0", "<")):
-			return bin2hex (openssl_random_pseudo_bytes($length));
+			return bin2hex(openssl_random_pseudo_bytes($length));
 		else:
 			return bin2hex(random_bytes($length));
 		endif;
